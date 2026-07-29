d53bb91b682e3511a1051b0678928c3c58b0e21e
lcoppenr <40072517+lcoppenr@users.noreply.github.com>
2026-07-29 12:55:13 -0700

    automation.h: ESPHome 2026.6 Action API compatibility
    9x 'void play(Ts... x)' -> 'void play(const Ts &...x)' per the ESPHome
    2026.6 automation Action API change. Pure API compat; upstream-worthy.

--- components/truma_inetbox/automation.h
+++ components/truma_inetbox/automation.h
@@ -18,7 +18,7 @@ template<typename... Ts> class HeaterRoomTempAction : public Action<Ts...>, publ
     this->heating_mode_ = TemplatableValue<HeatingMode, Ts...>(heating_mode);
   }
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_room(this->temperature_.value_or(x..., 0),
                                                     this->heating_mode_.value_or(x..., HeatingMode::HEATING_MODE_OFF));
   }
@@ -28,7 +28,7 @@ template<typename... Ts> class HeaterWaterTempAction : public Action<Ts...>, pub
  public:
   TEMPLATABLE_VALUE(uint8_t, temperature)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_water(this->temperature_.value_or(x..., 0));
   }
 };
@@ -42,7 +42,7 @@ template<typename... Ts> class HeaterWaterTempEnumAction : public Action<Ts...>,
     this->temperature_ = TemplatableValue<TargetTemp, Ts...>(temperature);
   }
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_water(this->temperature_.value_or(x..., TargetTemp::TARGET_TEMP_OFF));
   }
 };
@@ -51,7 +51,7 @@ template<typename... Ts> class HeaterElecPowerLevelAction : public Action<Ts...>
  public:
   TEMPLATABLE_VALUE(u_int16_t, watt)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_electric_power_level(this->watt_.value_or(x..., 0));
   }
 };
@@ -69,7 +69,7 @@ template<typename... Ts> class HeaterEnergyMixAction : public Action<Ts...>, pub
     this->watt_ = TemplatableValue<ElectricPowerLevel, Ts...>(watt);
   }
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_heater()->action_heater_energy_mix(
         this->energy_mix_.value_or(x..., EnergyMix::ENERGY_MIX_GAS),
         this->watt_.value_or(x..., ElectricPowerLevel::ELECTRIC_POWER_LEVEL_0));
@@ -80,14 +80,14 @@ template<typename... Ts> class AirconManualTempAction : public Action<Ts...>, pu
  public:
   TEMPLATABLE_VALUE(uint8_t, temperature)
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_aircon_manual()->action_set_temp(this->temperature_.value_or(x..., 0));
   }
 };
 
 template<typename... Ts> class TimerDisableAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  void play(Ts... x) override { this->parent_->get_timer()->action_timer_disable(); }
+  void play(const Ts &...x) override { this->parent_->get_timer()->action_timer_disable(); }
 };
 
 template<typename... Ts> class TimerActivateAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
@@ -113,7 +113,7 @@ template<typename... Ts> class TimerActivateAction : public Action<Ts...>, publi
     this->watt_ = TemplatableValue<ElectricPowerLevel, Ts...>(watt);
   }
 
-  void play(Ts... x) override {
+  void play(const Ts &...x) override {
     this->parent_->get_timer()->action_timer_activate(
         this->start_.value(x...), this->stop_.value(x...), this->room_temperature_.value(x...),
         this->heating_mode_.value_or(x..., HeatingMode::HEATING_MODE_OFF), this->water_temperature_.value_or(x..., 0),
@@ -125,7 +125,7 @@ template<typename... Ts> class TimerActivateAction : public Action<Ts...>, publi
 #ifdef USE_TIME
 template<typename... Ts> class WriteTimeAction : public Action<Ts...>, public Parented<TrumaiNetBoxApp> {
  public:
-  void play(Ts... x) override { this->parent_->get_clock()->action_write_time(); }
+  void play(const Ts &...x) override { this->parent_->get_clock()->action_write_time(); }
 };
 #endif  // USE_TIME
 
